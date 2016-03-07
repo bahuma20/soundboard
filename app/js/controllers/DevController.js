e40c728e127f83d2f48705b48d46e114e7164611
Max Bachhuber <bahuma20@bahuma.io>
2016-03-07 20:05:56 +0100

    Improved the add page

--- app/js/controllers/DevController.js
+++ app/js/controllers/DevController.js
@@ -1,15 +1,18 @@
 angular.module('soundboardApp').controller('DevController', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-  $scope.entry = {
+  var entryBoilerplate = {
     file: '',
     playCount: 0,
     title: '',
     new: true
   };
 
+  $scope.entry = angular.copy(entryBoilerplate);
+
   var ref = new Firebase('https://bahuma-soundboard.firebaseio.com/soundboards/-KC7Il71Qx1_qMglteqp/entries');
   $scope.entries = new $firebaseArray(ref);
 
   $scope.saveEntry = function(entry) {
     $scope.entries.$add(entry);
+    $scope.entry = angular.copy(entryBoilerplate);
   };
 }]);
